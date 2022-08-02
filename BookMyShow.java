f76344bf96d31dd8e24768e91efd0e5363efffe9
Pramod Yadav <pramodyadav.er@gmail.com>
2022-08-02 13:40:45 +0530

    initial commit

---
+++ BookMyShow.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class BookMyShow
+{
+    private ArrayList<User>users;
+    private ArrayList<Threater>threaters;
+    public BookMyShow() {
+        this.users = new ArrayList<>();
+        this.threaters = new ArrayList<>();
+    }
+
+}
